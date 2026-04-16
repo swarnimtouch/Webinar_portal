c0a0e68b77dc75797ae55667caeb5f740e489d3f
Prem <prem.swarnimtouch@gmail.com>
2026-04-16 15:21:53 +0530

    events changes

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -192,30 +192,22 @@
                     {
                         data: 'id', orderable: false, searchable: false,
                         render: id =>
-                            `<div class="dropdown">
-                        <a href="#" class="btn btn-light btn-active-light-primary btn-sm"
-                           data-bs-toggle="dropdown" aria-expanded="false">
-                            Actions
-                            <span class="svg-icon svg-icon-5 m-0">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584
-                                             5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929
-                                             15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25
-                                             10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358
-                                             8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533
-                                             13.0468 11.7467 13.0468 11.4343 12.7344Z"
-                                          fill="currentColor"/>
-                                </svg>
-                            </span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end py-4">
-                            <a href="{{ route('admin.events.add_edit_form') }}/${id}"
-                               class="dropdown-item px-3">Edit</a>
-                            <a href="#" class="dropdown-item px-3 text-danger event-delete"
-                               data-id="${id}">Delete</a>
-                        </div>
-                    </div>`
+                            `<div>
+                                                    <a href="#" class="btn btn-light btn-active-light-primary btn-sm" data-bs-toggle="dropdown"> Actions
+                                                        <span class="svg-icon svg-icon-5 m-0">
+                                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="currentColor"/> </svg>
+                                                            </span>
+                                                    </a>
+                                                    <div class="dropdown-menu dropdown-menu-end menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4">
+                                                        <div class="menu-item px-3">
+                                                            <a href="#" class="menu-link px-3 event-delete" data-id="${id}"> Delete </a>
+                                                        </div>
+                                                        <div class="menu-item px-3">
+                                                            <a href="{{ route('admin.events.add_edit_form') }}/${id}" class="menu-link px-3 "> Edit </a>
+                                                        </div>
+                                                    </div>
+                                            </div>`
                     }
                 ]
             });
@@ -287,7 +279,7 @@
                 buttonsStyling: false
             }).then(result => {
                 if (!result.isConfirmed) {
-                    checkbox.checked = !checkbox.checked; // revert
+                    checkbox.checked = !checkbox.checked;
                     return;
                 }
 
