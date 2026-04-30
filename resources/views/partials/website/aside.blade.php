3ec7ba7803823aab0d538ad5d70c1ac1a8f22af0
meetswarnimtouch <meet.swarnimtouch@gmail.com>
2026-04-30 15:34:12 +0530

    Update aside.blade.php

--- resources/views/partials/website/aside.blade.php
+++ resources/views/partials/website/aside.blade.php
@@ -177,6 +177,12 @@
                         <ul class="hands-list" id="handsList"></ul>
                     </div>
                 </div>
+                    <li>
+                        <a href="{{ route('category.index') }}">
+                            <i class="fa fa-list"></i>
+                            <span>Category</span>
+                        </a>
+                    </li>
 
             </div>
         </div>
