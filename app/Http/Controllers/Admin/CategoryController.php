4a5ab53c9933d5d28d63f6c9711cd4cce81b6b07
meetswarnimtouch <meet.swarnimtouch@gmail.com>
2026-04-30 13:50:34 +0530

    Update CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,63 +2,58 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
-class CategoryController
+class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $categories = Category::latest()->get();
+        return view('admin.category.index', compact('categories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('admin.category.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        Category::create([
+            'name' => $request->name,
+            'slug' => strtolower(str_replace(' ','-',$request->name)),
+        ]);
+
+        return redirect()->route('category.index')->with('success','Category Added');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function edit($id)
     {
-        //
+        $category = Category::find($id);
+        return view('admin.category.edit', compact('category'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $category = Category::find($id);
+
+        $category->update([
+            'name' => $request->name,
+            'slug' => strtolower(str_replace(' ','-',$request->name)),
+        ]);
+
+        return redirect()->route('category.index')->with('success','Updated');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        Category::find($id)->delete();
+        return back()->with('success','Deleted');
     }
 }
