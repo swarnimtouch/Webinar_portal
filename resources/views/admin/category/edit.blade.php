895be47b69ea49abcd992cc1615ab474cdd68daf
meetswarnimtouch <meet.swarnimtouch@gmail.com>
2026-04-30 13:56:44 +0530

    Update edit.blade.php

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,1 +1,16 @@
 
+@extends('admin.layout.app')
+
+@section('content')
+
+<form method="POST" action="{{ route('category.update',$category->id) }}">
+@csrf
+@method('PUT')
+
+<input type="text" name="name" value="{{ $category->name }}" class="form-control">
+
+<button class="btn btn-success mt-2">Update</button>
+
+</form>
+
+@endsection
