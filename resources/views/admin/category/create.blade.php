3313bc2bbe207c199e4eba9ff88b482bdfebe773
meetswarnimtouch <meet.swarnimtouch@gmail.com>
2026-04-30 13:56:26 +0530

    Update create.blade.php

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,1 +1,15 @@
 
+@extends('admin.layout.app')
+
+@section('content')
+
+<form method="POST" action="{{ route('category.store') }}">
+@csrf
+
+<input type="text" name="name" class="form-control" placeholder="Category Name">
+
+<button class="btn btn-success mt-2">Save</button>
+
+</form>
+
+@endsection
