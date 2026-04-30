d5411a07ca2202399f9f9773bc2e68cea1bb09ff
meetswarnimtouch <meet.swarnimtouch@gmail.com>
2026-04-30 13:56:01 +0530

    Update index.blade.php

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,1 +1,34 @@
 
+@extends('admin.layout.app')
+
+@section('content')
+
+<a href="{{ route('category.create') }}" class="btn btn-primary">Add Category</a>
+
+<table class="table mt-3">
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>Action</th>
+</tr>
+
+@foreach($categories as $cat)
+<tr>
+<td>{{ $cat->id }}</td>
+<td>{{ $cat->name }}</td>
+<td>
+<a href="{{ route('category.edit',$cat->id) }}" class="btn btn-info">Edit</a>
+
+<form action="{{ route('category.destroy',$cat->id) }}" method="POST" style="display:inline;">
+@csrf
+@method('DELETE')
+<button class="btn btn-danger">Delete</button>
+</form>
+
+</td>
+</tr>
+@endforeach
+
+</table>
+
+@endsection
