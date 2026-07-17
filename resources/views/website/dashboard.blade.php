8d88a5e6f902cdd5da8222721997ee88ec65ea4b
premswarnimtouch-cmyk <prem.swarnimtouch@gmail.com>
2026-07-17 12:07:57 +0530

    changes in mobile view

--- resources/views/website/dashboard.blade.php
+++ resources/views/website/dashboard.blade.php
@@ -12,12 +12,6 @@
                 <h1>{{ app('event')->name }}</h1>
 
 
-                <div class="mobile-chat-button-container">
-                    <button class="mobile-chat-btn" id="mobileChatBtn">
-                        <i class="fa-solid fa-comments"></i> Live Chat
-                    </button>
-                </div>
-
                 @php $hasAbout = filled(strip_tags((string) app('event')->description)); @endphp
                 @if($hasAbout || $resources->isNotEmpty())
                 <div class="webinar-info-container {{ $hasAbout ? '' : 'no-about' }}">
